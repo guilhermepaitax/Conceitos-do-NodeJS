690aef75ef00a333dd2d4366d754bfcb3aff731f
guilherme_paitax <[guilhermepaitax16@gmail.com]>
2019-08-20 14:51:08 -0300

    Adicionando rotas necessárias

--- index.js
+++ index.js
@@ -2,6 +2,30 @@ const express = require("express");
 
 const server = express();
 
+server.use(express.json());
+
 const projects = [];
+
+server.post("/projects", (req, res) => {
+  const { id, title } = req.body;
+
+  projects.push({
+    id,
+    title,
+    tasks: []
+  });
+
+  return res.json(projects);
+});
+
+server.get("/projects", (req, res) => {
+  return res.json(projects);
+});
+
+server.put("/projects/:id", (req, res) => {});
+
+server.delete("/projects/:id", (req, res) => {});
+
+server.post("/projects/:id/tasks", (req, res) => {});
 
 server.listen(3000);
